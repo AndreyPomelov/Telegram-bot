577e450a7cdc5b86053cdf8af97a9381595e1e24
Dmitriy Shcherbina <on@idimon.ru>
2022-01-23 22:50:22 +0300

    Рефакторинг и доработка бота
    #69 Добавлено оповещение пользовтелей
    #72 Рефакторинг добавления команд бота
    #67 Исправлена ошибка записи пользователя в базу, даже если он есть

--- telegram-bot/src/main/java/ru/gb/telegrambot/service/UserService.java
+++ telegram-bot/src/main/java/ru/gb/telegrambot/service/UserService.java
@@ -1,28 +1,35 @@
 package ru.gb.telegrambot.service;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.gb.telegrambot.domain.User;
 import ru.gb.telegrambot.repository.UserRepository;
 
-@Getter
-@Setter
-//@RequiredArgsConstructor
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class UserService  {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     public void saveUser(User user) {
         userRepository.save(user);
     }
+    public List<User>  findAll() {
+        return userRepository.findAll();
+    }
 
+    public Optional<User> findByChatId(Long id) {
+        return userRepository.findByTelegramId(id);
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
 
 }
